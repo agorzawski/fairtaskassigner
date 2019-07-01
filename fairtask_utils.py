3276193b3a9f1566447138d66e614982ad0c0882
Arek Gorzawski <agorzawski@gmail.com>
2019-07-01 22:07:45 +0200

    Statistics for charts

--- fairtask_utils.py
+++ fairtask_utils.py
@@ -10,8 +10,7 @@ def combineEvents(allJobs, badgesTimeline):
                          "<img src=\'%s\'/ width=\"25\", height=\"25\"> %s " % (oneBadge[3], oneBadge[2]),0))
     sortedToReturn = sorted(toReturn, key=lambda x: x[0], reverse=True)
 
-    # dictToReturn = {}  # valid for table-time-line
-    dictToReturn2 = {}  # valid for time-line
+    dictToReturn = {}  # valid for time-line
     localIndex = 0
     lastDateYYMMDD = None
     sum = 0
@@ -20,33 +19,25 @@ def combineEvents(allJobs, badgesTimeline):
         dateYYMMDD = one[0].split(' ')[0]
         dateArray = dateYYMMDD.split('-')
 
-        # dictToReturn[index] = {'date': {'all': one[0],
-        #                                 'year': dateArray[0],
-        #                                 'month': dateArray[1],
-        #                                 'day': dateArray[2], },
-        #                      'who': one[1],
-        #                      'towhom': one[2],
-        #                      'what': one[3]
-        # }
-
         if lastDateYYMMDD != dateYYMMDD:
-            dictToReturn2[dateYYMMDD] = {}
+            dictToReturn[dateYYMMDD] = {}
             localIndex = 0
             if index > 0:
-                dictToReturn2[lastDateYYMMDD]['total'] = sum
+                dictToReturn[lastDateYYMMDD]['total'] = sum
+                dictToReturn[lastDateYYMMDD]['all'] = one[0]
                 pass
             sum = one[4]
         if dateYYMMDD == lastDateYYMMDD or lastDateYYMMDD is None:
             localIndex += 1
             sum += one[4]
-        dictToReturn2[dateYYMMDD][localIndex] = {'date': {'all': one[0],
-                                                 'year': dateArray[0],
-                                                 'month': dateArray[1],
-                                                 'day': dateArray[2], },
-                                     'who': one[1],
-                                     'towhom': one[2],
-                                     'what': one[3]
+        dictToReturn[dateYYMMDD][localIndex] = {'date': {'all': one[0],
+                                                         'year': dateArray[0],
+                                                         'month': dateArray[1],
+                                                         'day': dateArray[2], },
+                                                'who': one[1],
+                                                'towhom': one[2],
+                                                'what': one[3],
         }
         lastDateYYMMDD = dateYYMMDD
 
-    return dictToReturn2
+    return dictToReturn
